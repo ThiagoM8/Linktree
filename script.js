8159b0ac7667c8f4e245b4c38400d49b86e883cd
ThiagoM8 <thiagomendesc998@gmail.com>
2025-08-04 09:50:17 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function togglemode() {
+ const body = document.body
+
+ /*if(body.classList.contains('light')) {
+    body.classList.remove('light')
+ } else {
+    body.classList.add('light')
+ }*/
+    
+  body.classList.toggle('light')  
+}
